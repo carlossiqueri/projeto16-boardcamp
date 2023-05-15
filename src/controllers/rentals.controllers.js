b64e57b454b24084392f149a00f84e3dd36abdeb
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-15 19:19:39 -0300

    Configs deploy

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,4 +1,4 @@
-import { db } from "../database/database.connection.js;
+import { db } from "../database/database.connection.js";
 import dayjs from "dayjs";
 
 export const postRental = async (req, res) => {
