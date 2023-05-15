d5469bd0245ba37bef8e6cba958c937a2b91ae80
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-15 19:24:12 -0300

    Configs deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = {
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
 if (process.env.MODE === "prod") configDatabase.ssl = true;
 
-export const db = new Pool(connection);
+export const db = new Pool(configDatabase);
