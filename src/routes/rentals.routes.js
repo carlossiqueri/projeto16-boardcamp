1927271ad5e33f58856bf9e93ac03b4975e68983
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-15 19:00:58 -0300

    Configs deploy

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { postRental } from "../controllers/rentals.controllers";
-import { rentalMiddleware } from "../middlewares/rentals.middlewares";
+import { postRental } from "../controllers/rentals.controllers.js";
+import { rentalMiddleware } from "../middlewares/rentals.middlewares.js";
 
 const rentalRouter = Router();
 
